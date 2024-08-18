a56495caae25481dc729ad5700d089f73e5b896f
Vanessa Rinaldi <vane.rinaldif4@gmail.com>
2024-08-18 20:00:02 -0300

    Solución click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,17 @@
 document.querySelector('div').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+
+
+
+const boton = document.getElementById('miBoton');
+const div = document.getElementById('miDiv');
+
+boton.addEventListener('click', function() {
+    event.stopPropagation(); 
+    alert('Hola');
+});
+
+div.addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
